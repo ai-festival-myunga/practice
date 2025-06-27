c73158d3ae478a09d29211504f1f2703e2b97fe4
kbsoo <kbsoo0620@gmail.com>
2025-06-27 20:25:10 +0900

    hugging face 예제3

--- hugging_face.py
+++ hugging_face.py
@@ -5,7 +5,7 @@ from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 load_dotenv()
 
-client = InferenceClient("meta-llama/Llama-3.3-70B-Instruct")
+client = InferenceClient(provider="hf-inference", model="meta-llama/Llama-3.3-70B-Instruct")
 
 output = client.text_generation(
     "The capital of France is",
@@ -13,4 +13,38 @@ output = client.text_generation(
 )
 
 print(output)
+# %%
+import  os
+from huggingface_hub import InferenceClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+client = InferenceClient(provider="hf-inference", model="meta-llama/Llama-3.3-70B-Instruct")
+
+prompt="""<|begin_of_text|><|start_header_id|>user<|end_header_id|>
+The capital of France is<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+output = client.text_generation(
+    prompt,
+    max_new_tokens=100,
+)
+
+print(output)
+# %%
+import  os
+from huggingface_hub import InferenceClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+client = InferenceClient(provider="hf-inference", model="meta-llama/Llama-3.3-70B-Instruct")
+
+output = client.chat.completions.create(
+    messages=[
+        {"role": "user", "content": "The capital of France is"},
+    ],
+    stream=False,
+    max_tokens=1024,
+)
+print(output.choices[0].message.content)
 # %%
